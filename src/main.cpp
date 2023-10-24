01a98c6cb2d7e807fa653b2180983630af2b2be0
Fang Jin <fang.jin@pearson.com>
2023-10-24 13:00:14 -0400

    test watchdog

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,26 @@
 #define LED_G A2
 #define LED_B A1
 
+void watchdogSetup(void)
+{
+  cli();
+  asm("WDR");
+  WDTCSR |= (1 << WDCE) | (1 << WDE);
+  WDTCSR = (1 << WDE) | (1 << WDP3); // 4s / no interrupt, system reset
+  sei();
+}
+
 void setup(void)
 {
   Serial.begin(115200);
+  Serial.println("*******************");
+  Serial.println("Starts...");
+
   pinMode(LED_R, OUTPUT);
   pinMode(LED_G, OUTPUT);
   pinMode(LED_B, OUTPUT);
+
+  // watchdogSetup();
 }
 
 void lightup(void)
@@ -17,15 +31,19 @@ void lightup(void)
   digitalWrite(LED_R, HIGH);
   delay(1000);
   digitalWrite(LED_R, LOW);
+  delay(10);
   digitalWrite(LED_G, HIGH);
   delay(1000);
   digitalWrite(LED_G, LOW);
+  delay(10);
   digitalWrite(LED_B, HIGH);
   delay(1000);
   digitalWrite(LED_B, LOW);
+  delay(10);
 }
 
 void loop(void)
 {
   lightup();
+  Serial.println("lightup");
 }
