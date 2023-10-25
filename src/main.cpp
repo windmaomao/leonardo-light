7a7fd4ddbb83bd49e45e89e906ab9fd58eef93e5
Fang Jin <fang.jin@pearson.com>
2023-10-25 14:30:57 -0400

    add light sensor to turn on

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,17 @@
 #define LED_R A3
 #define LED_G A2
 #define LED_B A1
+#define LIGHT_PIN (8)
+
+#define LIGHTON_THRESHOLD (35)
 
 void watchdogSetup(void)
 {
   cli();
   wdt_reset();
   WDTCSR |= (1 << WDCE) | (1 << WDE);
-  WDTCSR = (1 << WDIE) | (0 << WDE) | (1 << WDP3) | (1 << WDP0); // 8s / interrupt, no system reset
+  // WDTCSR = (1 << WDIE) | (0 << WDE) | (1 << WDP3) | (1 << WDP0); // 8s / interrupt, no system reset
+  WDTCSR = (1 << WDIE) | (0 << WDE) | (1 << WDP2) | (1 << WDP1) | (1 << WDP0); // 2s / interrupt, no system reset
   sei();
 }
 
@@ -24,6 +28,8 @@ void setup(void)
   pinMode(LED_R, OUTPUT);
   pinMode(LED_G, OUTPUT);
   pinMode(LED_B, OUTPUT);
+
+  pinMode(LIGHT_PIN, INPUT);
 }
 
 void flash(uint8_t pin, int start, int stop)
@@ -40,9 +46,15 @@ void lightUp(void)
   flash(LED_R, 1000, 10);
   flash(LED_G, 1000, 10);
   flash(LED_B, 1000, 10);
-  flash(LED_R, 1000, 10);
-  flash(LED_G, 1000, 10);
-  flash(LED_B, 1000, 10);
+  flash(LED_R, 500, 10);
+  flash(LED_G, 500, 10);
+  flash(LED_B, 500, 10);
+  flash(LED_R, 200, 10);
+  flash(LED_G, 200, 10);
+  flash(LED_B, 200, 10);
+  flash(LED_R, 100, 10);
+  flash(LED_G, 100, 10);
+  flash(LED_B, 100, 10);
 }
 
 void sleepNow()
@@ -54,9 +66,24 @@ void sleepNow()
   sleep_disable();
 }
 
+int readLight()
+{
+  int light = analogRead(LIGHT_PIN);
+  Serial.println(light);
+  return light;
+}
+
 void loop(void)
 {
-  lightUp();
+  int c = readLight();
+  if (c < LIGHTON_THRESHOLD)
+  {
+    lightUp();
+  }
+  else
+  {
+    delay(5000);
+  }
   sleepNow();
 }
 
